8ef2a94b800abefaef1d06471205d51fcff82348
Josh Harrison <joshuaharrison@unr.edu>
2017-11-30 09:52:20 -0800

    Create ProcessOTUtable.R
    This R program takes an OTU table (csv) as input and outputs a TMM normalized table, and 1k, 5k, and 10k rarefied read tables. Can handle multiple files and piping...easy to tweak if you don't want to use a csv for some reason

---
+++ ProcessOTUtable.R
@@ -0,0 +1,56 @@
+#!/usr/bin/Rscript
+#J. G. Harrison
+
+#This script takes an OTU table (csv) as input where rows are samples, 
+#columns are otus, and cells are filled with raw read counts.
+#The output will include a TMM normalized OTU table (as per Mcurmdie and Holmes 2012)
+#and several rarified tables (1k, 5k, 10k reads), note in many cases you may
+#want to try analyses without samples with low coverage, these won't be rarefied of course.
+
+#Usage: 
+#Rscript ProcessOTUtable.R  OTUtable.csv OTUtable2.csv ...
+
+#for more info on using R outside of an IDE see https://swcarpentry.github.io/r-novice-inflammation/05-cmdline/
+
+#read stdin and argument to rarefy. Note that this allows for piping, and for multiple files
+main <- function() {
+  inargs <- commandArgs(trailingOnly = TRUE) #cuts out the calls to R that Rscript includes automatically, eg --slave --no-restore, etc
+  print(inargs)
+  if (length(inargs) == 0) {
+    process(file("stdin"))
+  } else {
+    for (f in inargs) {
+      process(f)
+    }
+  }
+}
+
+#Perform work on the input file(s)
+process <- function(filename) {
+  dat <- read.csv(file = filename, header = T) #here is where you could swap to tab separated.
+
+  #Normalize via the TMM method, RLE often fails with sparse data
+  fungi = edgeR::DGEList(counts=as.matrix(t(dat[,2:length(dat)])), group=as.matrix(dat[,1]))
+  
+  y=edgeR::calcNormFactors(fungi, method="TMM")
+  
+  #get normalized counts
+  nc=edgeR::cpm(y, normalized.lib.sizes=T)
+  
+  #make a filename that only has the file, not its whole path, this aids naming the out file
+  abrv_filename = basename(filename)
+  write.csv(t(nc), file=paste("./TMMnormalized_", abrv_filename, sep=""))
+  
+  write.csv(vegan::rarefy(dat[,2:length(dat)], sample=1000), 
+            file=paste("./1krarefied_", abrv_filename, sep=""))
+  write.csv(vegan::rarefy(dat[,2:length(dat)], sample=5000)
+            , file=paste("./5krarefied_", abrv_filename, sep=""))
+  write.csv(vegan::rarefy(dat[,2:length(dat)], sample=10000)
+             , file=paste("./10k_rarefied", abrv_filename, sep=""))
+  
+}
+
+main()  
+
+
+ 
