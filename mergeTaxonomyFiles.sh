0d6fdb18ab1c8522a264b865e38cc71e9f59af0d
Josh Harrison <jgharrison1982@gmail.com>
2018-01-26 17:21:03 -0700

    fixed bugs in sort

--- mergeTaxonomyFiles.sh
+++ mergeTaxonomyFiles.sh
@@ -6,19 +6,20 @@
 #Usage bash mergeTaxonomyFiles.sh file1 file2
 
 #make sure input files are in same order. For some reason sintax doesn't do this
-sort -k 1 $1 > $1
-sort -k 1 $2 > $2
+sort -k 1 $1 > tmp.txt
+sort -k 1 $2 > tmp2.txt
 
 #get the fourth field, which is the taxonomic assignment that was chosen at
 #>80% probability, based on kmer matches
-cut -f4 $1 > tmp.txt
-cut -f4 $2 > tmp2.txt
+cut -f4 tmp.txt > tax.txt
+cut -f4 tmp2.txt > tax2.txt
 
 #get OTU names, these should be the same, but will write both just in case
-cut -f1 $1 > namesFile1.txt
-cut -f1 $2 > namesFile2.txt
+cut -f1 tmp.txt > namesFile1.txt
+cut -f1 tmp2.txt > namesFile2.txt
 
-paste namesFile1.txt namesFile2.txt tmp.txt tmp2.txt > combinedTaxonomy.txt
+paste namesFile1.txt namesFile2.txt tax*txt > combinedTaxonomy.txt
 
 rm -rf tmp*
+rm -rf tax*txt
 rm -rf namesFile*
