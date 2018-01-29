2ed1fadfbe2e5309b2355d5656a7b1d5919c76db
Josh Harrison <jgharrison1982@gmail.com>
2018-01-29 14:51:43 -0700

    formatting

--- useTax_to_cleanOTUtable.R
+++ useTax_to_cleanOTUtable.R
@@ -2,7 +2,7 @@
 #J. G. Harrison
 #Jan. 26, 2018
 
-#this script takes an otu table a taxonomy file and a sample to well key and 
+#this script takes an otu table a taxonomy file and a sample to well key and
 #outputs an OTU table that does not have non-target taxa (e.g. no plants if you are doing fungi)
 
 #This script takes several inputs:
@@ -10,7 +10,7 @@
 #2. An otu table
 #3. key linking sample to well
 
-#NOTE: it does not remove cpDNA or mtDNA explicitly. 
+#NOTE: it does not remove cpDNA or mtDNA explicitly.
 
 #IMPORTANT: This script has very limited utility and very likely won't work for you unless you
 #tweak it a bit, or are using my pipeline, so carefully read through it!
@@ -20,7 +20,7 @@
 #Usage: Rscript useTax_to_cleanOTUtable.R taxonomyFileExample.txt OTUtableExample.txt Well_to_sampleKeyExample.csv
 
 main <- function() {
-  inargs <- commandArgs(trailingOnly = TRUE) 
+  inargs <- commandArgs(trailingOnly = TRUE)
   print(inargs)
 
   #input otu table, sample-well key, and taxonomy file
@@ -28,7 +28,7 @@ main <- function() {
   otus=read.delim(file=inargs[2], header=T)
   sampNames=read.csv(file=inargs[3])
   sampNames$pl_well=paste(sampNames$plate, sampNames$well, sep=".")
-  
+
   #now we need to pick which OTUs to keep
   #I am first removing any OTUs that the UNITE database said were plants.
   #then I will subset the data to just keep only those OTUs that one or the other database
@@ -36,10 +36,13 @@ main <- function() {
   #phylum often match nontarget taxa on NCBI...so I remove those too.
 
   fungiOnly= tax[tax[,4]!="d:Plantae",]
+  plants = tax[tax[,4]=="d:Plantae",]
 
-  #this keeps any row that for either database there is more than 7 characters in the cell
+  #this keeps any row that for either database there is more than 7 characters
+  #in the cell
   #describing the taxonomic hypothesis.
-  #this gets rid of anything that was not identified to phylum by one or the other db
+  #this gets rid of anything that was not identified to phylum by
+  #one or the other db.
 
   fungiOnly = fungiOnly[which(nchar(as.character(fungiOnly[,3])) > 10 | nchar(as.character(fungiOnly[,4])) > 10),]
 
@@ -71,7 +74,7 @@ main <- function() {
 
   #make sure to edit the range in the call to the newotus object
   write.csv(data.frame(newotus$sampleName, newotus[,grep("Otu",names(newotus))]),file="cleanOTUtable_youshouldrename.csv", row.names = F)
-  
+
 }
 
-main()  
+main()
