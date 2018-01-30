551c212da87f91b5b5d4bcef3e8fc3b5afcd036d
Josh Harrison <jgharrison1982@gmail.com>
2018-01-30 13:00:38 -0700

    made otu id not split between two fields

--- fastq_to_OTUtable.sh
+++ fastq_to_OTUtable.sh
@@ -281,7 +281,7 @@ usearch -otutab allstrip.fa -otus otus97.fa -maxrejects 1000 -otutabout out/otuT
 #note the -i.bak  flag which makes edits inline
 
 sed 's/#//' out/otuTable97otus.txt > interim.txt
-mv -f interim.txt out/otuTable97otus.txt
+sed 's/OTU\ ID/OTU_ID/' interim.txt > out/otuTable97otus.txt
 rm -rf interim.txt
 
 #bug in old usearch needs this hack
@@ -294,9 +294,9 @@ usearch -otutab allstrip.fa -otus zotus.fa -maxrejects 1000 -otutabout out/otuTa
 
 #remove the hash from the first line. For some reason usearch puts this there
 sed 's/#//' out/otuTableZotus.txt > interim.txt
-mv -f interim.txt out/otuTableZotus.txt
-rm -rf interim.txt
+sed 's/OTU\ ID/OTU_ID/' interim.txt > out/otuTableZotus.txt
 
+rm -rf interim.txt
 rm -rf allstrip.fq
 
 #additional quality control step. Make sure all OTUs made it into the table
