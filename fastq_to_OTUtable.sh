972980d317edadabbbb6d8e23f42df75e65bca1a
Josh Harrison <jgharrison1982@gmail.com>
2018-01-30 11:48:42 -0700

    fixed bug introduced by usearch that made field names not output

--- fastq_to_OTUtable.sh
+++ fastq_to_OTUtable.sh
@@ -3,20 +3,20 @@
 #I copied the pipeline from the USEARCH documentation and heavily edited
 #J. Harrison Jan 16, 2018
 #
-# 
+#
 # #IMPORTANT PLEASE READ!!!
 # #This script will output a summary txt called Processing_Summary in a dir called out/
 # #within the working dir. This has important QC info so check it out.
 # #This script assumes one starts in the dir that has all unpacked fastqs to process
 # #The script requires a dir to be present called db/ that has a comparison database of your chosing
 # #You can change the path to this database in Step 3
-# 
-# #You will want to make sure the primer stripping step (step 4) has the correct number of 
+#
+# #You will want to make sure the primer stripping step (step 4) has the correct number of
 # #bases specified to be removed
 # #this will change depending on what primers you are using
-# 
+#
 # #This script assumes demultiplexed reads that are zipped (can comment out zipped option below)
-# 
+#
 # #YOU WILL NEED an extra script to be in the working dir.
 
 # #rmv_fungal_matches.pl
@@ -45,13 +45,13 @@ rm -rf out
 mkdir out
 touch out/Processing_Summary.txt
 
-gunzip *fastq.gz 
+gunzip *fastq.gz
 
 #######################################
 ####Step 1. Examine quality of reads
 #######################################
 
-#the main benefit to this step is to see how many reads get cut. 
+#the main benefit to this step is to see how many reads get cut.
 #really it can be skipped because of the filtering done later
 
 mkdir fastq_info
@@ -103,7 +103,7 @@ fi
 #######################################
 
 #Merge paired reads
-#Add sample name to read label (using the -relabel option). 
+#Add sample name to read label (using the -relabel option).
 #Sample name is just the file name
 #this defaults to 10 threads, or number of cores, whichever is less
 #I doubt you will need to run more cores here very often as this is not slow
@@ -112,7 +112,7 @@ for f in *R1*
 do
 	fname=$(basename $f)
 	fname2=${fname/R1/R2}
-	
+
 	usearch -fastq_mergepairs ${fname} -reverse ${fname2} -fastqout ${fname}.merged.fq -sample $fname
 
 done
@@ -133,9 +133,9 @@ usearch -orient testFiles.fq -db db/rdp_16s_v16_sp.fa -tabbedout orient_out.txt
 
 #this should show most reads are + or ? (the latter being ones that didn't match anything in the db)
 
-echo "Sequence orientation for the first 5 samples." >> out/Processing_Summary.txt 
+echo "Sequence orientation for the first 5 samples." >> out/Processing_Summary.txt
 
-orient=`eval cut -f2 orient_out.txt | sort | uniq -c`  
+orient=`eval cut -f2 orient_out.txt | sort | uniq -c`
 
 echo "$orient" >> out/Processing_Summary.txt
 
@@ -148,7 +148,7 @@ rm -rf testFiles.fq
 
 # Strip primers (V4F is 18, V4R is 20) (ITS1F is 22, ITS2R is 20)
 # this will be important to doublecheck, as this can vary by primer pair
- 
+
 for f in *merged.fq
 do
 	usearch -fastx_truncate $f -stripleft 18 -stripright 20 -fastqout ${f}stripped.fq
@@ -162,7 +162,7 @@ done
 
 #Note that we do this after merging so we can leverage info from both reads when calling bases
 #If you are using a sequence technology prone to errors, then best to rethink this step
-#or at least look into the output more carefully, 
+#or at least look into the output more carefully,
 #see https://www.drive5.com/usearch/manual/pipe_readprep_filter.html
 
 for f in *stripped.fq
@@ -215,9 +215,9 @@ echo "Number of zotus that passed min length requirements: <64 bp long" >> out/P
 echo `grep ">" zotuspre2.fa | wc -l` >> out/Processing_Summary.txt
 
 #Second, remove low complexity OTUS (ones filled with repeats)
-#note I am not saving hits or anything here. 
+#note I am not saving hits or anything here.
 
-usearch -filter_lowc otus97pre2.fa -output otus97pre3.fa 
+usearch -filter_lowc otus97pre2.fa -output otus97pre3.fa
 usearch -filter_lowc  zotuspre2.fa -output  zotuspre3.fa
 
 #number of Otus before QC steps
@@ -228,9 +228,9 @@ echo "Number of zotus that passed min complexity reqs. (OTUS with >25% low compl
 echo `grep ">" zotuspre3.fa | wc -l` >> out/Processing_Summary.txt
 
 #Remove any OTUs that are from PhiX (the Illumina internal standard). Likely won't lose much here for miseq
-#may be more problematic with Hiseq...keep an eye on this 
+#may be more problematic with Hiseq...keep an eye on this
 
-usearch -filter_phix otus97pre3.fa -output otus97.fa 
+usearch -filter_phix otus97pre3.fa -output otus97.fa
 usearch -filter_phix  zotuspre3.fa -output  zotus.fa
 
 #number of Otus before QC steps
@@ -264,7 +264,7 @@ rm -rf otus.aln
 # Step 8. Make OTU table
 ##################################################
 #Note the maxrejects option for the otutab function slows down that function a lot bc it reduces heuristics
-#it reduces the probability of errors however. 
+#it reduces the probability of errors however.
 #if speed becomes an issue for some reason, then reconsider this option.
 
 echo "About to cat this make take a minute, go get some coffee!"
@@ -277,6 +277,9 @@ usearch -fastq_filter allstrip.fq -fastaout allstrip.fa
 #make 97% OTU table
 usearch -otutab allstrip.fa -otus otus97.fa -maxrejects 1000 -otutabout out/otuTable97otus.txt -notmatched unmapped97.fa
 
+#remove the hash from the first line. For some reason usearch puts this there
+sed 's/#//' out/otuTable97otus.txt > out/otuTable97otus.txt
+
 #bug in old usearch needs this hack
 sed 's/Zotu/Otu/' zotus.fa > zotus2.fa
 rm -rf zotus.fa
@@ -285,10 +288,13 @@ mv zotus2.fa zotus.fa
 #make ZOTU table
 usearch -otutab allstrip.fa -otus zotus.fa -maxrejects 1000 -otutabout out/otuTableZotus.txt -notmatched unmappedZOTUS.fa
 
-rm -rf allstrip.fq 
+#remove the hash from the first line. For some reason usearch puts this there
+sed 's/#//' out/otuTableZotus.txt > out/otuTableZotus.txt 
+
+rm -rf allstrip.fq
 
 #additional quality control step. Make sure all OTUs made it into the table
-#check these missing OTUs, if there are any, to see what they are. 
+#check these missing OTUs, if there are any, to see what they are.
 #could be an error with the otutable function, could be an offset read that didnt get caught
 #could also be a strand duplicate (reverse compliment)
 
@@ -297,7 +303,7 @@ grep "^>" otus97.fa | sed "-es/>//" > otuTitles97.txt
 sort otuTitles97.txt included97s.txt included97s.txt | uniq -u > missing_labels97.txt
 usearch -fastx_getseqs otus97.fa -labels missing_labels97.txt -fastaout out/missing97otus.fa
 
-#here we find the notmissing OTUs then match the missing OTUs against them, this helps check for 
+#here we find the notmissing OTUs then match the missing OTUs against them, this helps check for
 #offsets or strand dupes
 
 sort missing_labels97.txt missing_labels97.txt otuTitles97.txt | uniq -u > notmissing_labels97.txt
@@ -328,46 +334,46 @@ rm -rf *aln
 
 	# ##################################################
 	# # Deprecated. The following bit used to be neccessary, but is no longer needed
-	# # I am leaving it in case memory becomes a problem and this is needed again 
+	# # I am leaving it in case memory becomes a problem and this is needed again
 	# ##################################################
-	# 
+	#
 	# #dont use filtered reads bc we can recover some info from them when using 97% match
-	# 
+	#
 	# for f in *stripped.fq
 	# do
 	# 	usearch -usearch_global $f -db otus97.fa -id 0.97 -threads 32 -strand plus -uc ${f}_97.uc
 	# done
-	# 
+	#
 	# cat *97.uc > all_ucs_97.uc
-	# 
+	#
 	# #This removes sequences that didn't match anything
 	# perl rmv_matches.pl all_ucs_97.uc
-	# 
+	#
 	# mv all_matches.uc all_matches97.uc
-	# 
+	#
 	# #Do same thing for zotus
 	# #note search exact doesn't work for some reason...Not sure why
-	# 
+	#
 	# for f in *merged.fq
 	# do
 	# 	usearch -usearch_global $f -db zotus.fa -id 1 -threads 32 -strand plus -uc ${f}_zotus.uc
 	# done
-	# 
+	#
 	# cat *zotus.uc > all_ucs_zotus.uc
-	# 
+	#
 	# #This removes sequences that didn't match anything
 	# perl rmv_matches.pl all_ucs_zotus.uc
-	# 
+	#
 	# mv all_matches.uc all_matchesZOTUs.uc
-	# 
+	#
 	# # ##################################################
 	# # # Step 9. make OTU table
 	# # ##################################################
-	# 
+	#
 	# #this writes the OTU tables to the out/ dir
-	# 
+	#
 	# Rscript uc_to_OTUtable.R all_matchesZOTUs.uc all_matches97.uc
-	
+
 	#End deprecated section
 
 
@@ -410,10 +416,10 @@ echo `grep ">" zotus.fa | wc -l` >> out/Processing_Summary.txt
 # # ##################################################
 
  rm -rf *merged*
- 
+
  gzip *fastq
  mkdir rawreads
- 
+
  mv *fastq.gz rawreads
  mv otus97.fa out/
  mv zotus.fa out/
@@ -422,8 +428,6 @@ echo `grep ">" zotus.fa | wc -l` >> out/Processing_Summary.txt
  rm -rf all_ucs*uc
  rm -rf uniqueSequences.fa
  rm -rf combined_filtered.fa
- 
+
  tar -czf fastq_info.tar.gz  fastq_info/
  rm -rf fastq_info/
- 
-
